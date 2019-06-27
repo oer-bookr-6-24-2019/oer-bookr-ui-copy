92d4620408f277fa1ec3591afa62259bf8fc69d5
Alex <agordon1123@gmail.com>
2019-06-27 13:04:36 -0400

    file restructuring

--- JS/index.js
+++
@@ -1,6 +0,0 @@
-const registerBtn = document.querySelector('.registerBtn');
-console.log(registerBtn);
-registerBtn.addEventListener('mousedown', event => {
-  console.log('listening');
-  event.target.style = 'margin-top: +10px';
-});
